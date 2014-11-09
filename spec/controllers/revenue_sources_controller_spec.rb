79221384751b361d030b37585c00512dbfe33c9c
Richard Newman <richardnewman@gogolabs.net>
2014-11-09 01:11:55 -0700

    more complete spec tests

--- spec/controllers/revenue_sources_controller_spec.rb
+++ spec/controllers/revenue_sources_controller_spec.rb
@@ -22,12 +22,18 @@ RSpec.describe RevenueSourcesController, type: :controller do
   end
 
   describe "'POST' create" do
-    it 'succeeds if well formed input' do
+    it 'succeeds if well formed input and returns to list' do
       post :create, revenue_source: FactoryGirl.attributes_for(:revenue_source)
       expect(response).to redirect_to revenue_sources_path
     end
 
-    it 'fails if poorly formed input' do
+    it 'adds a record' do
+      expect {
+        post :create, revenue_source: FactoryGirl.attributes_for(:revenue_source)
+      }.to change{RevenueSource.count}.by(1)
+    end
+
+    it 'fails if no input supplied' do
       expect {
         post :create, revenue_source: nil
       }.to raise_error ActionController::ParameterMissing
