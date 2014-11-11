1534a7184502d0c93e022b46f9e5582cec476374
Richard Newman <richardnewman@gogolabs.net>
2014-11-10 21:42:47 -0700

    responsive list implementation with drop down detail mechanism

--- app/assets/javascripts/react/revenue_sources.js.jsx
+++ app/assets/javascripts/react/revenue_sources.js.jsx
@@ -1,5 +1,58 @@
 /** @jsx React.DOM */
 
+/*var RevenueSourceListItem = React.createClass({
+  getInitialState: function() {
+    return { color: 'black' };
+  },
+
+  _onClick: function() {
+    this.setState({color: 'green' });
+
+
+    //this.style.color = green;
+    //this.setState({ color: 'green' });
+  },
+
+  render: function() {
+    return <li id={this.state.id} onClick={this._onClick} style={ {color: this.state.color} }>{this.props.item.name}</li>;
+  }
+});
+*/
+
+var RevenueSourceDetail = React.createClass({
+  render: function() {
+    if (this.props.showDetail) {
+      return (
+        <div>
+          hello
+        </div>
+      )
+    } else {
+      return ( null )
+    }
+  }
+});
+
+var RevenueSourceListItem = React.createClass({
+  getInitialState: function() {
+    return { showDetail: false };
+  },
+
+  _onClick: function() {
+    this.setState({showDetail: this.state.showDetail ? false : true });
+  },
+
+  render: function() {
+    var _display = this.state.showDetail ? null : { display: 'none' };
+    return (
+      <li onClick={this._onClick}>
+        {this.props.item.name}
+        <RevenueSourceDetail showDetail={this.state.showDetail}/>
+      </li>
+    )
+  }
+});
+
 var RevenueSourcesSection = React.createClass({
   getInitialState: function() {
     return { data: [] };
@@ -29,16 +82,12 @@ var RevenueSourcesSection = React.createClass({
   },
 
   render: function() {
-    var items = [];
-    var data  = this.state.data;
-    for (var i = 0; i < data.length; i++) {
-      items.push(<li>{data[i].name}</li>);
-    }
-    return <ul>{items}</ul>;
+    return (
+      <ul>
+      {this.state.data.map(function(item) {
+        return <RevenueSourceListItem key={item.id} item={item}/>;
+      })}
+      </ul>
+    );
   }
 });
-
-
-
-
-
