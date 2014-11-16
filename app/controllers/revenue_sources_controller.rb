c9d5a0b578437c0b6f824da9d8972aaf29818352
Richard Newman <richardnewman@gogolabs.net>
2014-11-15 21:52:30 -0700

    improved spec testing on controller

--- app/controllers/revenue_sources_controller.rb
+++ app/controllers/revenue_sources_controller.rb
@@ -15,30 +15,42 @@ class RevenueSourcesController < ApplicationController
 
   def create
     _params = revenue_source_params
-    begin
-      RevenueSource.create(_params)
-      flash[:success] = "Added #{_params[:name]}."
-    rescue StandardError => e
-      Rails.logger.error "[RevenueSourcesController#create] failed, error = #{e.inspect}"
-      flash[:error] = "Unable to add #{_params[:name]}."
+    if _params.empty?
+      flash[:error] = "Unable to add; client error."
+      redirect_to revenue_sources_path, status: :bad_request
+    else
+      begin
+        RevenueSource.create(_params)
+        flash[:success] = "Added #{_params[:name]}."
+      rescue StandardError => e
+        Rails.logger.error "[RevenueSourcesController#create] failed, error = #{e.inspect}"
+        flash[:error] = "Unable to add #{_params[:name]}."
+      end
+      redirect_to revenue_sources_path, status: :created
     end
-    redirect_to revenue_sources_path
   end
 
   def update
     _row = RevenueSource.find_by_id(params[:id])
-Rails.logger.info "_row = #{_row.inspect}"
     _params = revenue_source_params
-    begin
-      _row.update_attributes(_params)
-      flash[:success] = "Updated #{_params[:name]}."
-    rescue StandardError => e
-      Rails.logger.error "[RevenueSourcesController#update] failed, error = #{e.inspect}"
-      flash[:error] = "Unable to update #{_params[:name]}."
-    end
-    respond_with do |format|
-      format.json { render json: _row }
-      format.html { redirect_to revenue_sources_path }
+    if _params.empty?
+      flash[:error] = "Unable to update #{_params[:name]} due to client error."
+      respond_with do |format|
+        format.json { render json: _row, status: :bad_request }
+        format.html { redirect_to revenue_sources_path, status: :bad_request }
+      end
+    else
+      begin
+        _row.update_attributes(_params)
+        flash[:success] = "Updated #{_params[:name]}."
+      rescue StandardError => e
+        Rails.logger.error "[RevenueSourcesController#update] failed, error = #{e.inspect}"
+        flash[:error] = "Unable to update #{_params[:name]}."
+      end
+      respond_with do |format|
+        format.json { render json: _row }
+        format.html { redirect_to revenue_sources_path }
+      end
     end
   end
 
